933b9b7269b3bd1a3ad6a1fdff70edc83e44db8b
Sebastian Waisbrot <seppo0010@gmail.com>
2016-08-21 18:10:17 -0300

    Add handle test

--- src/handle.rs
+++ src/handle.rs
@@ -12,6 +12,7 @@ impl <D: Database> HandlerData<D> {
     }
 }
 
+#[derive(Clone)]
 pub enum Method {
     Ping,
 }
@@ -38,4 +39,70 @@ pub trait Res: Default {
 
 pub fn handle<ReqT: Req, ResT: Res, D: Database, Data: AsRef<HandlerData<D>>>(req: &ReqT, res: &mut ResT, d: Data) {
     req.method().handle(req, res, d)
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+    use std::sync::{Arc, Mutex};
+
+    use futures::{IntoFuture, Done};
+
+    use database::{Database, Error};
+
+    use super::{Method, Req, Res, HandlerData, handle};
+
+    struct Request {
+        method: Method,
+    }
+
+    impl Request {
+        fn new(method: Method) -> Self {
+            Request {
+                method: method,
+            }
+        }
+    }
+
+    impl Req for Request {
+        fn method(&self) -> Method { self.method.clone() }
+    }
+
+    #[derive(Default)]
+    struct Response {
+        pong_response: bool,
+    }
+
+    impl Res for Response {
+        fn set_pong_response(&mut self) {
+            self.pong_response = true;
+        }
+    }
+
+    #[derive(Default)]
+    struct MockDatabase {
+        data: Mutex<HashMap<Vec<u8>, Vec<u8>>>,
+    }
+
+    impl Database for MockDatabase {
+        fn put(&self, key: &[u8], value: &[u8]) -> Done<(), Error> {
+            self.data.lock().unwrap().insert(key.to_vec(), value.to_vec());
+            Ok(()).into_future()
+        }
+
+        fn get(&self, key: &[u8]) -> Done<Option<Vec<u8>>, Error> {
+            Ok(self.data.lock().unwrap().get(key).cloned()).into_future()
+        }
+    }
+
+    #[test]
+    fn test_ping() {
+        let request = Request::new(Method::Ping);
+        let mut response = Response::default();
+        let database = MockDatabase::default();
+        let data = HandlerData::new(database);
+        assert!(!response.pong_response);
+        handle(&request, &mut response, Arc::new(data));
+        assert!(response.pong_response);
+    }
 }
