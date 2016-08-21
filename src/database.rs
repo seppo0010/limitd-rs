0a702d19342e31fe5c172ac6be1f767877522f17
Sebastian Waisbrot <seppo0010@gmail.com>
2016-08-21 17:56:30 -0300

    Add tests to database

--- src/database.rs
+++ src/database.rs
@@ -2,11 +2,11 @@ use std::path::Path;
 
 use leveldb;
 use leveldb::database::Database as LDatabase;
-use leveldb::options::{Options, WriteOptions};
+use leveldb::options::{Options, ReadOptions, WriteOptions};
 use leveldb::kv::KV;
 use db_key::Key as DBKey;
 
-use futures::{failed, finished, Future};
+use futures::{IntoFuture, Done};
 
 // Not quite a fan of this implementation
 pub struct Key {
@@ -35,7 +35,8 @@ impl From<leveldb::error::Error> for Error {
 }
 
 pub trait Database: Send + Sync + 'static {
-    fn put(&self, key: &[u8], value: &[u8]) -> Box<Future<Item=(), Error=Error>>;
+    fn put(&self, key: &[u8], value: &[u8]) -> Done<(), Error>;
+    fn get(&self, key: &[u8]) -> Done<Option<Vec<u8>>, Error>;
 }
 
 pub struct LevelDB<K: DBKey> {
@@ -52,11 +53,57 @@ impl<K: DBKey> LevelDB<K> {
 }
 
 impl<K: DBKey + 'static> Database for LevelDB<K> {
-    fn put(&self, key: &[u8], value: &[u8]) -> Box<Future<Item=(), Error=Error>> {
+    fn put(&self, key: &[u8], value: &[u8]) -> Done<(), Error> {
         let write_opts = WriteOptions::new();
-        match self.db.put(write_opts, K::from_u8(key), value) {
-            Ok(_) => { return finished(()).boxed() },
-            Err(e) => { return failed(Error::LevelDBError(e)).boxed() }
-        }
+        self.db.put(write_opts, K::from_u8(key), value).map_err(|e| Error::LevelDBError(e)).into_future()
+    }
+
+    fn get(&self, key: &[u8]) -> Done<Option<Vec<u8>>, Error> {
+        let read_opts = ReadOptions::new();
+        self.db.get(read_opts, K::from_u8(key)).map_err(|e| Error::LevelDBError(e)).into_future()
+    }
+}
+
+#[cfg(test)]
+mod test{
+    extern crate tempdir;
+
+    use futures::Future;
+    use super::{Database, Key, LevelDB};
+
+    #[test]
+    fn put_get() {
+        let db = LevelDB::<Key>::new(tempdir::TempDir::new("put_get").unwrap().path().to_str().unwrap()).unwrap();
+        let key = [0, 1, 2, 3];
+        let value = [4, 6, 8, 10];
+        db.put(&key, &value).and_then(move |_| {
+            db.get(&key).map(move |v| {
+                assert_eq!(v.unwrap(), &value);
+            })
+        });
+    }
+
+    #[test]
+    fn put_put_get() {
+        let db = LevelDB::<Key>::new(tempdir::TempDir::new("put_get").unwrap().path().to_str().unwrap()).unwrap();
+        let key = [0, 1, 2, 3];
+        let value = [4, 6, 8, 10];
+        let value2 = [100, 101, 102, 103];
+        db.put(&key, &value).and_then(move |_| {
+            db.put(&key, &value2).and_then(move |_| {
+                db.get(&key).map(move |v| {
+                    assert_eq!(v.unwrap(), &value2);
+                })
+            })
+        });
+    }
+
+    #[test]
+    fn get_none() {
+        let db = LevelDB::<Key>::new(tempdir::TempDir::new("get_none").unwrap().path().to_str().unwrap()).unwrap();
+        let key = [0, 1, 2, 3];
+        db.get(&key).map(move |v| {
+            assert!(v.is_none());
+        });
     }
 }
